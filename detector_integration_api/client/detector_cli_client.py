4787377bdb7479092f539f772f07374925cafa8d
Andrej Babic <andrej.babic@psi.ch>
2017-09-14 15:13:45 +0200

    Detector CLI stub

--- detector_integration_api/client/detector_cli_client.py
+++ detector_integration_api/client/detector_cli_client.py
@@ -1,2 +1,12 @@
 class DetectorClient(object):
-    pass
+    def acquire(self):
+        pass
+
+    def get(self, parameter_name):
+        pass
+
+    def put(self, parameter_name, value):
+        pass
+
+    def set_config(self, configuration):
+        pass
