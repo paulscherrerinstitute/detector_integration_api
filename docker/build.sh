93d046e96521fcbc9b770a4fe5d8692906b0ddc8
Andrej Babic <andrej.babic@psi.ch>
2017-09-19 17:59:54 +0200

    Increase lib version

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-VERSION=0.0.1
+VERSION=0.0.2
+SLS_DETECTOR_VERSION=3.0
+
+sh get_sls_detectors.sh ${SLS_DETECTOR_VERSION}
+
 docker build --no-cache=true -t docker.psi.ch:5000/detector_api_server .
 docker tag docker.psi.ch:5000/detector_api_server docker.psi.ch:5000/detector_api_server:$VERSION
 docker push docker.psi.ch:5000/detector_api_server:$VERSION
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="0.0.1",
+      version="0.0.2",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
