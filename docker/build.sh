23be48e89e9c9ad4f1cb6cfb68eef610a245f03f
Andrej Babic <andrej.babic@psi.ch>
2017-09-21 15:54:35 +0200

    Change docker build procedure

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 VERSION=0.0.3
-SLS_DETECTOR_VERSION=3.0
+SLS_DETECTOR_VERSION=3.0.1
+GIT_USER=git
 
-sh get_sls_detectors.sh ${SLS_DETECTOR_VERSION}
+# Clone the sls_detector_software
+git clone ${GIT_USER}@git.psi.ch:sls_detectors_software/sls_detectors_package.git sls_detectors_package
+cd sls_detectors_package
+sh checkout.sh $GIT_USER
+sh gitall.sh checkout $SLS_DETECTOR_VERSION
+cd -
 
+# Build the docker image.
 docker build --no-cache=true -t docker.psi.ch:5000/detector_api_server .
+
+# Push it to our repo.
 docker tag docker.psi.ch:5000/detector_api_server docker.psi.ch:5000/detector_api_server:$VERSION
 docker push docker.psi.ch:5000/detector_api_server:$VERSION
 docker push docker.psi.ch:5000/detector_api_server
