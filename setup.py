e3d11ba4630a746761e05e091f7a77eeef354c4e
Andrej Babic <andrej.babic@psi.ch>
2017-09-26 18:17:22 +0200

    New library version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="0.0.4",
+      version="0.0.5",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
