8c0d515e0b80aeb5bbdf43e68c6ab0caddb53da9
Andrej Babic <andrej.babic@psi.ch>
2017-09-28 18:07:51 +0200

    Fix rest client code

--- detector_integration_api/rest_api/rest_client.py
+++ detector_integration_api/rest_api/rest_client.py
@@ -16,62 +16,62 @@ class DetectorIntegrationClient(object):
     def start(self):
         request_url = self.api_address + routes["start"]
 
-        response = requests.post(request_url)
+        response = requests.post(request_url).json()
 
         return validate_response(response)
 
     def stop(self):
         request_url = self.api_address + routes["stop"]
 
-        response = requests.post(request_url)
+        response = requests.post(request_url).json()
 
         return validate_response(response)
 
     def get_status(self):
         request_url = self.api_address + routes["get_status"]
 
-        response = requests.get(request_url)
+        response = requests.get(request_url).json()
 
         return validate_response(response)
 
     def get_status_details(self):
         request_url = self.api_address + routes["get_status_details"]
 
-        response = requests.get(request_url)
+        response = requests.get(request_url).json()
 
         return validate_response(response)
 
     def get_config(self):
         request_url = self.api_address + routes["get_config"]
 
-        response = requests.get(request_url)
+        response = requests.get(request_url).json()
 
         return validate_response(response)
 
     def set_config(self, configuration):
         request_url = self.api_address + routes["set_config"]
 
-        response = requests.put(request_url, json=configuration)
+        response = requests.put(request_url, json=configuration).json()
 
         return validate_response(response)
 
     def update_config(self, configuration):
         request_url = self.api_address + routes["update_config"]
 
-        response = requests.post(request_url, json=configuration)
+        response = requests.post(request_url, json=configuration).json()
 
         return validate_response(response)
 
     def reset(self):
         request_url = self.api_address + routes["reset"]
 
-        response = requests.post(request_url)
+        response = requests.post(request_url).json()
 
         return validate_response(response)
 
     def get_server_info(self):
         request_url = self.api_address + routes["get_server_info"]
 
-        response = requests.get(request_url)
+        response = requests.get(request_url).json()
 
         return validate_response(response)
