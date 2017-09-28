d9b4ab5afcb82ce2f20a2ab8f23c8e308acfecf2
Andrej Babic <andrej.babic@psi.ch>
2017-09-28 18:42:02 +0200

    Detector cmd command bug fix

--- detector_integration_api/client/detector_cli_client.py
+++ detector_integration_api/client/detector_cli_client.py
@@ -21,16 +21,16 @@ class DetectorClient(object):
         return raw_status
 
     def _get(self, parameter_name):
-        cli_command = "sls_detector_get %s" % parameter_name
-        _logger.debug("Executing get command: '%s'.", cli_command)
+        cli_command = ["sls_detector_get", parameter_name]
+        _logger.debug("Executing get command: '%s'.", " ".join(cli_command))
 
         cli_result = subprocess.check_output(cli_command)
         # TODO: Validate cli_result
         return cli_result
 
     def _put(self, parameter_name, value):
-        cli_command = "sls_detector_put %s %s" % (parameter_name, value)
-        _logger.debug("Executing put command: '%s'.", cli_command)
+        cli_command = ["sls_detector_put", parameter_name, value]
+        _logger.debug("Executing put command: '%s'.", " ".join(cli_command))
 
         cli_result = subprocess.check_output(cli_command)
         # TODO: Validate cli_result
