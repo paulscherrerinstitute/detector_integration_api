116aa11e775a9e53df3bf47b99a911c16758e948
Andrej Babic <andrej.babic@psi.ch>
2017-10-10 14:46:42 +0200

    New library version
    - debug start and stop functions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="0.0.8",
+      version="0.0.9",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
