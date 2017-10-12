c97f8cd2fbba92a1c94612065c1569fe3222956d
Andrej Babic <andrej.babic@psi.ch>
2017-10-12 17:13:34 +0200

    New library version
    - Improved debug mode
    - Fixed docker startup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="0.1.0",
+      version="0.1.1",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
