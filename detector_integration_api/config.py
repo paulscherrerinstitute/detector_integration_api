d074713f28bd3a4f51f087c5bc5dc9a79bc1ced0
Andrej Babic <andrej.babic@psi.ch>
2017-10-18 15:46:53 +0200

    Fix default client URLs

--- detector_integration_api/config.py
+++ detector_integration_api/config.py
@@ -2,8 +2,8 @@ DEFAULT_LOGGING_LEVEL = "DEBUG"
 DEFAULT_SERVER_INTERFACE = "0.0.0.0"
 DEFAULT_SERVER_PORT = 10000
 
-DEFAULT_BACKEND_URL = "http://localhost:8080/v1"
-DEFAULT_WRITER_URL = "http://localhost:8083/api/v1/"
+DEFAULT_BACKEND_URL = "http://localhost:8080"
+DEFAULT_WRITER_URL = "http://localhost:8083"
 DEFAULT_WRITER_INSTANCE_NAME = "writer"
 DEFAULT_VALIDATION_MODULE = "detector_integration_api.validation.debug"
 
