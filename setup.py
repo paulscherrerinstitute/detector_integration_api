ab1b91783c581b8992f035260dec53b5c59e5de9
Andrej Babic <andrej.babic@psi.ch>
2017-11-16 14:36:54 +0100

    New library version
    - Waiting status for the detector

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="0.1.4",
+      version="0.1.5",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
