1a62c944b7f1ed584e16126aa27ce39cb4a93f4f
Andrej Babic <andrej.babic@psi.ch>
2017-11-22 15:05:13 +0100

    Make SF specific validator

--- detector_integration_api/config.py
+++ detector_integration_api/config.py
@@ -5,7 +5,7 @@ DEFAULT_SERVER_PORT = 10000
 DEFAULT_BACKEND_URL = "http://localhost:8080"
 DEFAULT_WRITER_URL = "http://localhost:8083"
 DEFAULT_WRITER_INSTANCE_NAME = "writer"
-DEFAULT_VALIDATION_MODULE = "detector_integration_api.validation.debug"
+DEFAULT_VALIDATION_MODULE = "detector_integration_api.validation.sf_validator"
 DEFAULT_MANAGER_MODULE = "detector_integration_api.manager.sf_manager"
 
 BACKEND_URL_SUFFIX = "/v1"
