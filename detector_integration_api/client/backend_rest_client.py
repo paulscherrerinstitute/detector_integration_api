01bdb5d2bd670788b4bf0461173168bfbc66ca29
Andrej Babic <andrej.babic@psi.ch>
2018-01-29 11:46:46 +0100

    Add backend communication timeout

--- detector_integration_api/client/backend_rest_client.py
+++ detector_integration_api/client/backend_rest_client.py
@@ -12,7 +12,8 @@ class BackendClient(object):
         self.backend_url = backend_url.rstrip("/") + config.BACKEND_URL_SUFFIX
 
     def open(self):
-        response_text = requests.post(self.backend_url + "/state/open", json={}).text
+        response_text = requests.post(self.backend_url + "/state/open", json={},
+                                      timeout=config.BACKEND_COMMUNICATION_TIMEOUT).text
 
         _logger.debug("Opening backend got %s" % response_text)
 
@@ -24,7 +25,8 @@ class BackendClient(object):
     def close(self):
         _logger.debug("Stopping backend.")
 
-        response_text = requests.post(self.backend_url + "/state/close", json={}).text
+        response_text = requests.post(self.backend_url + "/state/close", json={},
+                                      timeout=config.BACKEND_COMMUNICATION_TIMEOUT).text
 
         _logger.debug("Response from backend: %s" % response_text)
 
@@ -32,19 +34,22 @@ class BackendClient(object):
             raise ValueError("Cannot stop backend, aborting: %s" % response_text)
 
     def get_status(self):
-        return requests.get(self.backend_url + "/state").json()["global_state"]
+        return requests.get(self.backend_url + "/state",
+                            timeout=config.BACKEND_COMMUNICATION_TIMEOUT).json()["global_state"]
 
     def reset(self):
         _logger.debug("Resetting backend.")
 
-        response_text = requests.post(self.backend_url + "/state/reset", json={}).text
+        response_text = requests.post(self.backend_url + "/state/reset", json={},
+                                      timeout=config.BACKEND_COMMUNICATION_TIMEOUT).text
 
         _logger.debug("Response from backend: %s" % response_text)
 
     def set_config(self, configuration):
         _logger.debug("Configuring backend.")
 
-        response_text = requests.post(self.backend_url + "/state/configure", json={"settings": configuration}).text
+        response_text = requests.post(self.backend_url + "/state/configure", json={"settings": configuration},
+                                      timeout=config.BACKEND_COMMUNICATION_TIMEOUT).text
 
         _logger.debug("Response from backend %s" % response_text)
 
@@ -54,7 +59,8 @@ class BackendClient(object):
     def get_metrics(self, metrics=["received_frames", "sent_frames"]):
         _logger.debug("Getting backend metrics.")
         # TODO: do a default selection here
-        answer = requests.get(self.backend_url + "/metrics").json()["value"]["backend"]
+        answer = requests.get(self.backend_url + "/metrics",
+                              timeout=config.BACKEND_COMMUNICATION_TIMEOUT).json()["value"]["backend"]
         # selecting answers
         if metrics != []:
             ret = {k: answer.get(k, None) for k in metrics}
