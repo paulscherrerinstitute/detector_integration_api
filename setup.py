4b2fb4a5fe6e109d80905d51a9e9235ff64a5ce4
Andrej Babic <andrej.babic@psi.ch>
2018-02-19 09:26:06 +0100

    New library version
    - Removed implementations from repository.
    - Minor refactoring.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="0.2.3",
+      version="1.0.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
