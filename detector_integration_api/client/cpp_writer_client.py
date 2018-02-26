62b0c8db5315551fad6925a3b972cfb47099b0d0
Andrej Babic <andrej.babic@psi.ch>
2018-02-26 18:01:30 +0100

    Add writer executable and log_folder config

--- detector_integration_api/client/cpp_writer_client.py
+++ detector_integration_api/client/cpp_writer_client.py
@@ -15,12 +15,21 @@ _logger = getLogger(__name__)
 class CppWriterClient(object):
     PROCESS_STARTUP_PARAMETERS = ("output_file", "n_frames", "user_id")
 
-    def __init__(self, stream_url, writer_port):
+    def __init__(self, stream_url, writer_executable, writer_port, log_folder=None):
 
-        self.writer_port = writer_port
-        self.url = config.WRITER_PROCESS_URL_FORMAT % writer_port
         self.stream_url = stream_url
+        self.writer_executable = writer_executable
+        self.writer_port = writer_port
 
+        self.log_folder = None
+        if log_folder is not None:
+
+            if not os.path.exists(log_folder):
+                raise ValueError("Provided writer log folder '%s' does not exist." % log_folder)
+
+            self.log_folder = log_folder
+
+        self.writer_url = config.WRITER_PROCESS_URL_FORMAT % writer_port
         self.writer_parameters = None
 
         self.process = None
@@ -40,22 +49,27 @@ class CppWriterClient(object):
             raise ValueError("Writer parameters not set.")
 
         timestamp = datetime.now().strftime(config.WRITER_PROCESS_LOG_FILENAME_TIME_FORMAT)
-        log_filename = os.path.join(config.WRITER_PROCESS_LOG_DIR,
-                                    config.WRITER_PROCESS_LOG_FILENAME_FORMAT % timestamp)
 
-        writer_command_format = "sh /home/dia/start_writer.sh %s %s %s %s %s"
-        writer_command = writer_command_format % (self.stream_url,
-                                                  self.writer_parameters["output_file"],
-                                                  self.writer_parameters.get("n_frames", 0),
-                                                  self.writer_port,
-                                                  self.writer_parameters.get("user_id", -1))
-        _logger.debug("Starting writer with command '%s'.", writer_command)
+        # If the log folder is not specified, redirect the logs to /dev/null.
+        if self.log_folder is not None:
+            log_filename = os.path.join(self.log_folder,
+                                        config.WRITER_PROCESS_LOG_FILENAME_FORMAT % timestamp)
+        else:
+            log_filename = os.devnull
 
         _logger.debug("Creating log file '%s'.", log_filename)
         self.process_log_file = open(log_filename, 'w')
         self.process_log_file.write("Parameters:\n%s\n" % json.dumps(self.writer_parameters, indent=4))
         self.process_log_file.flush()
 
+        writer_command_format = "sh " + self.writer_executable + " %s %s %s %s %s"
+        writer_command = writer_command_format % (self.stream_url,
+                                                  self.writer_parameters["output_file"],
+                                                  self.writer_parameters.get("n_frames", 0),
+                                                  self.writer_port,
+                                                  self.writer_parameters.get("user_id", -1))
+
+        _logger.debug("Starting writer with command '%s'.", writer_command)
         self.process = Popen(writer_command, shell=True, stdout=self.process_log_file, stderr=self.process_log_file)
 
         sleep(config.WRITER_PROCESS_STARTUP_WAIT_TIME)
@@ -66,7 +80,7 @@ class CppWriterClient(object):
         for _ in range(config.WRITER_PROCESS_RETRY_N):
 
             try:
-                response = requests.post(self.url + "/parameters", json=process_parameters,
+                response = requests.post(self.writer_url + "/parameters", json=process_parameters,
                                          timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
 
                 if response.status_code != 200:
@@ -79,7 +93,7 @@ class CppWriterClient(object):
         else:
             _logger.warning("Terminating writer process because it did not respond in the specified time.")
 
-            requests.get(self.url + "/kill", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
+            requests.get(self.writer_url + "/kill", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
 
             self.process.wait(timeout=config.WRITER_PROCESS_TERMINATE_TIMEOUT)
 
@@ -92,7 +106,7 @@ class CppWriterClient(object):
         _logger.debug("Stopping writer.")
 
         if self.is_running():
-            requests.get(self.url + "/stop", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
+            requests.get(self.writer_url + "/stop", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
 
             try:
                 self.process.wait(timeout=config.WRITER_PROCESS_TERMINATE_TIMEOUT)
@@ -100,7 +114,7 @@ class CppWriterClient(object):
             except:
                 _logger.warning("Terminating writer process because it did not stop in the specified time.")
 
-                requests.get(self.url + "/kill", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
+                requests.get(self.writer_url + "/kill", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
 
                 self.process.wait(timeout=config.WRITER_PROCESS_TERMINATE_TIMEOUT)
 
@@ -126,7 +140,7 @@ class CppWriterClient(object):
 
         # Writer is running. Get status from the process.
         if self.is_running():
-            status = requests.get(self.url + "/status", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT).json()
+            status = requests.get(self.writer_url + "/status", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT).json()
 
             return status["status"]
 
@@ -147,4 +161,4 @@ class CppWriterClient(object):
         if not self.is_running():
             return {}
 
-        return requests.get(self.url + "/statistics", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT).json()
+        return requests.get(self.writer_url + "/statistics", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT).json()
