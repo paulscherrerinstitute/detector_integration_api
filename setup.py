802a597ea18cbc1fa3ed5adf53bb664d5825e2c1
Andrej Babic <andrej.babic@psi.ch>
2019-01-24 15:56:14 +0100

    New library version
    - Add parameters to DIA start
    
    Warning: Changes are not compatible with old version of SF_DIA.
    Please update sf_dia as well.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="1.5.3",
+      version="1.6.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
