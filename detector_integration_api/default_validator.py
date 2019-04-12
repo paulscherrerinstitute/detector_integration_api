c48cd96a133e540a29d1aa4c140cfbea09b7eaa3
Andrej Babic <andrej.babic@psi.ch>
2019-04-12 11:33:15 +0200

    Create default validator
    The default validator provides a set of default
    validation procedures to initiate check configs.
    In addition, the example validator is removed,
    since the default validator acts as an example at
    the same time.

--- detector_integration_api/default_validator.py
+++ detector_integration_api/default_validator.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from detector_integration_api.utils import validate_mandatory_parameters
+from detector_integration_api.utils import validate_mandatory_parameters, compare_client_status
 
 
 class IntegrationStatus(Enum):
@@ -12,13 +12,8 @@ class IntegrationStatus(Enum):
 
 E_ACCOUNT_USER_ID_RANGE = [10000, 29999]
 
-
 MANDATORY_WRITER_CONFIG_PARAMETERS = {
     "n_frames": int, "user_id": int, "output_file": str
-}
-
-MANDATORY_BACKEND_CONFIG_PARAMETERS = {
-    "bit_depth": int
 }
 
 
@@ -38,8 +33,62 @@ def validate_writer_config(configuration):
         configuration["output_file"] += ".h5"
 
 
+MANDATORY_BACKEND_CONFIG_PARAMETERS = {
+    "bit_depth": int
+}
+
+
 def validate_backend_config(configuration):
     try:
         validate_mandatory_parameters(configuration, MANDATORY_BACKEND_CONFIG_PARAMETERS)
     except Exception as ex:
         raise ValueError("Invalid backend configuration") from ex
+
+
+MANDATORY_DETECTOR_CONFIG_PARAMETERS = {
+    "period": float, "frames": int, "dr": int
+}
+
+
+def validate_detector_config(configuration):
+    try:
+        validate_mandatory_parameters(configuration, MANDATORY_DETECTOR_CONFIG_PARAMETERS)
+    except Exception as ex:
+        raise ValueError("Invalid detector configuration") from ex
+
+
+def validate_configs_dependencies(writer_config, backend_config, detector_config):
+    if backend_config["bit_depth"] != detector_config["dr"]:
+        raise ValueError("Invalid config. Backend 'bit_depth' set to '%s', but detector 'dr' set to '%s'."
+                         " They must be equal." % (backend_config["bit_depth"], detector_config["dr"]))
+
+    if detector_config["timing"] == "auto":
+        if detector_config["frames"] != writer_config["n_frames"]:
+            raise ValueError("Invalid config for timing auto. "
+                             "Detector 'n_frames' set to '%s', but writer 'n_frames' set to '%s'."
+                             " They must be equal."
+                             % (detector_config["n_frames"], writer_config["n_frames"]))
+
+    elif detector_config["timing"] == "trigger" or detector_config["timing"] == "gating":
+        if detector_config["cycles"] != writer_config["n_frames"]:
+            raise ValueError("Invalid config for timing trigger. "
+                             "Detector 'cycles' set to '%s', but writer 'n_frames' set to '%s'."
+                             " They must be equal."
+                             % (detector_config["cycles"], writer_config["n_frames"]))
+    else:
+        raise ValueError("Unexpected detector timing config '%s'. Use 'timing' or 'auto'." % detector_config["timing"])
+
+
+def interpret_status(statuses):
+    writer = statuses["writer"]
+
+    # If no other conditions match.
+    interpreted_status = IntegrationStatus.ERROR
+
+    if compare_client_status(writer, "stopped"):
+        interpreted_status = IntegrationStatus.READY
+
+    elif compare_client_status(writer, "writing"):
+        interpreted_status = IntegrationStatus.RUNNING
+
+    return interpreted_status
