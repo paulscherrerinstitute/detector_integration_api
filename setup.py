482da58a20bdb447117c7bb09238b4c5e6f14c7b
Andrej Babic <andrej.babic@psi.ch>
2019-08-09 11:00:38 +0200

    New library version
    - Merge from dia_python_config

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="1.6.1",
+      version="1.7.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
