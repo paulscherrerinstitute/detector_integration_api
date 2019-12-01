d6bc0958853abdc010653a31d4a909f16cb4cace
Dmitry Ozerov <Dmitry.Ozerov@psi.ch>
2019-12-01 08:55:05 +0100

    new version 1.8.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="detector_integration_api",
-      version="1.8.0",
+      version="1.8.1",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
